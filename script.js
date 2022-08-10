3cf348613bc7d32f6a2b9a39f2382e2fba76c608
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-10 20:02:22 +0300

    initial display of the calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const operator = (op, a, b) => {
+  return op(a, b);
+};
