da86b82fa0bd8913aef1c8f5c4da004184976feb
semihgurbuz <semihgurbuz.dev@gmail.com>
2022-08-15 20:07:53 +0300

    calculator is functional for the most part

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Basic operations
+
 const add = (a, b) => {
   return a + b;
 };
@@ -14,6 +16,146 @@ const divide = (a, b) => {
   return a / b;
 };
 
-const operator = (op, a, b) => {
+const operator = (op = op, a = topDisplayNumber, b = mainDisplayNumber) => {
   return op(a, b);
 };
+
+//a function to create classes and add event listeners quickly
+
+const button = document.querySelectorAll("button");
+let mainDisplay = document.querySelector(".current");
+
+let topDisplay = document.querySelector(".previous");
+
+let mainDisplayNumber;
+let topDisplayNumber;
+let op;
+function addClass(button) {
+  for (let i = 0; i < button.length; i++) {
+    let element = button[i];
+    let class1 = element.innerHTML;
+    element.classList.add(class1);
+    element.addEventListener("click", () => {
+      if (
+        class1 != "Clear" &&
+        class1 != "=" &&
+        class1 != "x" &&
+        class1 != "/" &&
+        class1 != "+" &&
+        class1 != "-" &&
+        class1 != "."
+      ) {
+        mainDisplay.innerHTML += class1;
+        mainDisplayNumber = parseFloat(mainDisplay.innerHTML);
+      }
+    });
+  }
+}
+
+addClass(button);
+
+let dot = document.querySelector(".dot");
+dot.addEventListener("click", () => {
+  if (mainDisplay.innerHTML.includes(".") != true) {
+    mainDisplay.innerHTML += ".";
+    mainDisplayNumber = parseFloat(mainDisplay.innerHTML);
+  }
+});
+
+let clear = document.querySelector(".Clear");
+clear.addEventListener("click", () => {
+  mainDisplay.innerHTML = "";
+  topDisplay.innerHTML = "";
+});
+
+let x = document.querySelector(".x");
+x.addEventListener("click", () => {
+  if (topDisplay.innerHTML.length === 0) {
+    op = multiply;
+    topDisplay.innerHTML = mainDisplayNumber + " x";
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" x", ""));
+    mainDisplay.innerHTML = "";
+  } else {
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" x", ""));
+    topDisplay.innerHTML = equal() + " x";
+    mainDisplay.innerHTML = "";
+  }
+});
+
+let add1 = document.querySelector(".add");
+add1.addEventListener("click", () => {
+  if (topDisplay.innerHTML.length === 0) {
+    op = add;
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" +", ""));
+    topDisplay.innerHTML = mainDisplayNumber + " +";
+    mainDisplay.innerHTML = "";
+  } else {
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" +", ""));
+    topDisplay.innerHTML = equal() + " +";
+    mainDisplay.innerHTML = "";
+  }
+});
+
+let sub = document.querySelector(".subtract");
+sub.addEventListener("click", () => {
+  if (topDisplay.innerHTML.length === 0) {
+    op = subtract;
+    topDisplay.innerHTML = mainDisplayNumber + " -";
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" -", ""));
+    mainDisplay.innerHTML = "";
+  } else {
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" -", ""));
+    topDisplay.innerHTML = equal() + " -";
+    mainDisplay.innerHTML = "";
+  }
+});
+
+let div = document.querySelector(".divide");
+div.addEventListener("click", () => {
+  if (topDisplay.innerHTML.length === 0) {
+    op = divide;
+    topDisplay.innerHTML = mainDisplayNumber + " /";
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" /", ""));
+    mainDisplay.innerHTML = "";
+  } else {
+    topDisplayNumber = parseFloat(topDisplay.innerHTML.replace(" /", ""));
+    topDisplay.innerHTML = equal() + " /";
+    mainDisplay.innerHTML = "";
+  }
+});
+
+let equals = document.querySelector(".equals");
+equals.addEventListener("click", () => {
+  if (mainDisplay.innerHTML != "") {
+    mainDisplay.innerHTML = equal();
+    mainDisplayNumber = parseFloat(mainDisplay.innerHTML);
+  }
+});
+
+const equal = () => {
+  if (topDisplay.innerHTML.includes("x") || topDisplay.innerHTML.length === 0) {
+    op = multiply;
+  } else if (
+    topDisplay.innerHTML.includes("+") ||
+    topDisplay.innerHTML.length === 0
+  ) {
+    op = add;
+  } else if (
+    topDisplay.innerHTML.includes("/") ||
+    topDisplay.innerHTML.length === 0
+  ) {
+    op = divide;
+  } else if (
+    topDisplay.innerHTML.includes("-") ||
+    topDisplay.innerHTML.length === 0
+  ) {
+    op = subtract;
+  }
+  let result = operator(op, topDisplayNumber, mainDisplayNumber);
+  result = parseFloat((Math.round(result * 100) / 100).toFixed(3));
+  topDisplayNumber = result;
+  return result;
+};
+
+console.log(topDisplayNumber);
+console.log(mainDisplayNumber);
